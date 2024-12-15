2b709a9227a3c08651af3f6356e8e4199347fb37
Matthew Peairs <25410392+pointfivetee@users.noreply.github.com>
2024-12-15 17:35:51 -0500

    Allow plant buds to spawn even if the player is carrying the corresponding item

---
+++ Patches/PlantPatch.cs
@@ -0,0 +1,17 @@
+using BepInEx5ArchipelagoPluginTemplate.templates;
+using HarmonyLib;
+
+// Soil patches that normally grow a unique checked item don't check the inventory before spawning
+[HarmonyPatch(typeof(PlantController), "Grow")]
+class PlantPatch
+{
+    static void Prefix(PlantController __instance)
+    {
+        var crop = new Traverse(__instance).Field("crop").GetValue<CropObject>();
+        var itemObject = crop.DoNotSpawnIfHaveThisItemObject;
+        if (itemObject && Plugin.ItemIdToLocation.ContainsKey(itemObject.Index))
+        {
+            crop.DoNotSpawnIfHaveThisItemObject = null;
+        }
+    }
+}
