a47f908ad9992f1230d24d3c6c387711e7ba4e42
Matthew Peairs <25410392+pointfivetee@users.noreply.github.com>
2024-12-15 17:35:50 -0500

    Add logging on pickup; add remaining items to dict

---
+++ Patches/CollisionPatch.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using BepInEx5ArchipelagoPluginTemplate.templates;
+using BepInEx5ArchipelagoPluginTemplate.templates.Archipelago;
+using BepInEx5ArchipelagoPluginTemplate.templates.Utils;
+using HarmonyLib;
+
+// Patches the method called when the player steps on a pickup
+[HarmonyPatch(typeof(PlayerController), nameof(PlayerController.OnTriggerStay2D))]
+class CollisionPatch
+{
+    // Maps in-game item ID (e.g., "active_watering") to its vanilla location name as defined in the
+    // apworld (e.g, "Veggieville - Steal From Lemon").
+    static Dictionary<string, string> itemIdToLocation = new Dictionary<string, string>()
+    {
+        // Veggieville
+        {"active_watering", "Veggieville - Steal From Lemon"},
+        {"key_flower", "Veggieville - Water Florist's Flower"},
+        // Weapon Woods
+        {"active_sword", "Weapon Woods - Soil Sword Patch"},
+        {"key_money", "Weapon Woods - Murder Jerry"},
+        // Layer Lane
+        {"key_trophy", "Layer Lane - Trophy Corner"},
+        {"key_sub", "Layer Lane - Sandwich Stall"},
+        // Bustling Barn
+        {"hat_crown", "Bustling Barn - slayQueen32 Reward"},
+        {"key_boots", "Bustling Barn - Boom Boots Room"},
+        {"key_fertilizer", "Bustling Barn - Past The King Pig"},
+    };
+
+    static bool enablePatch = true;
+
+    static bool Prefix(UnityEngine.Collider2D _collider2D, PlayerController __instance)
+    {
+        if (enablePatch)
+        {
+            if (__instance.CanMove() && _collider2D.gameObject.CompareTag("Item"))
+            {
+                ItemController component = _collider2D.gameObject.GetComponent<ItemController>();
+                if (component != null)
+                {
+                    var itemObject = component.GetItemObject();
+                    string itemId = itemObject.Index;
+                    ArchipelagoConsole.LogMessage("Item Collision! " + itemObject.GetName() + " " + itemId);
+                    if (itemIdToLocation.ContainsKey(itemId))
+                    {
+                        // We still need to destroy the item
+                        component.Pickup();
+                        // Report the location as collected and skip the default trigger
+                        ArchipelagoConsole.LogMessage("Reporting collection of " + itemIdToLocation[itemId]);
+                        Plugin.ArchipelagoClient.CollectFrom(itemIdToLocation[itemId]);
+                        // TODO: The item that was originally here needs to be flagged as picked up, so it won't respawn
+
+                        return false;
+                    }
+                }
+            }
+
+            // Not a location tracked by Archipelago - proceed normally
+            return true;
+        }
+
+        // Proceed normally
+        return true;
+    }
+}
--- Patches/PickupPatch.cs
+++ Patches/PickupPatch.cs
@@ -3,47 +3,15 @@ using BepInEx5ArchipelagoPluginTemplate.templates;
 using BepInEx5ArchipelagoPluginTemplate.templates.Utils;
 using HarmonyLib;
 
-// Patches the method called when the player steps on a pickup
-[HarmonyPatch(typeof(PlayerController), nameof(PlayerController.OnTriggerStay2D))]
-class CollisionPatch
+// Do some logging on item pickup
+[HarmonyPatch(typeof(PlayerController), nameof(PlayerController.PickupItem))]
+class PickupPatch
 {
-    // Maps in-game item ID (e.g., "active_watering") to its vanilla location name as defined in the
-    // apworld (e.g, "Veggieville - Steal From Lemon").
-    // TODO: Add all currently supported locations
-    static Dictionary<string, string> itemIdToLocation = new Dictionary<string, string>() {
-        // Veggieville
-        {"active_watering", "Veggieville - Steal From Lemon"},
-        {"key_flower", "Veggieville - Water Florist's Flower"},
-    };
-
-    static bool Prefix(UnityEngine.Collider2D _collider2D, PlayerController __instance)
+    static bool Prefix(ItemObject _itemObject, bool _effect)
     {
-        if (__instance.CanMove() && _collider2D.gameObject.CompareTag("Item"))
-        {
-            ItemController component = _collider2D.gameObject.GetComponent<ItemController>();
-            if (component != null)
-            {
-                var itemObject = component.GetItemObject();
-                string itemId = itemObject.Index;
-                ArchipelagoConsole.LogMessage("Item Collision! " + itemObject.GetName() + " " + itemId);
-                if (itemIdToLocation.ContainsKey(itemId))
-                {
-                    // We still need to destroy the item
-                    component.Pickup();
-                    // Report the location as collected and skip the default trigger
-                    ArchipelagoConsole.LogMessage("Reporting collection of " + itemIdToLocation[itemId]);
-                    Plugin.ArchipelagoClient.CollectFrom(itemIdToLocation[itemId]);
-                    // TODO: The item that was originally here needs to be flagged as picked up, so it won't respawn
+        ArchipelagoConsole.LogMessage("Item Pickup! " + _itemObject.GetName() + " " + _itemObject.Index);
 
-                    return false;
-                }
-            }
-        }
-
-        // Not a location tracked by Archipelago - proceed normally
+        // Proceed normally
         return true;
     }
 }
-
-// How to get the PlayerController object in the absence of an __instance
-// Singleton<PlayerManager>.Instance.GetConnectedPlayer()
