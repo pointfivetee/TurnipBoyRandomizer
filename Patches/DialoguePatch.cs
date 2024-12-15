b8d646a2c58828f82f32671021d061c228e7dec4
Matthew Peairs <25410392+pointfivetee@users.noreply.github.com>
2024-12-15 17:35:51 -0500

    Add patch to handle checks awarded by dialogue

---
+++ Patches/DialoguePatch.cs
@@ -0,0 +1,35 @@
+using BepInEx5ArchipelagoPluginTemplate.templates;
+using BepInEx5ArchipelagoPluginTemplate.templates.Utils;
+using HarmonyLib;
+using UnityEngine;
+
+// Intercept a dialogue upon closing if it rewards an item
+[HarmonyPatch(typeof(DialoguePopupController), nameof(DialoguePopupController.Nuke))]
+class DialoguePatch
+{
+    static bool Prefix(DialoguePopupController __instance)
+    {
+        if (Plugin.EnableRandomization)
+        {
+            var dialogue = new Traverse(__instance).Field("dialogue").GetValue<Dialogue>();
+            if (dialogue.RewardItemObject != null && dialogue.RewardItemObject.CanSpawn())
+            {
+                var itemObject = dialogue.RewardItemObject;
+                string itemId = itemObject.Index;
+                if (Plugin.ItemIdToLocation.ContainsKey(itemId))
+                {
+                    // Perform the normal cleanup
+                    Singleton<GameManager>.Instance.LosePopup();
+                    Object.Destroy(__instance.gameObject);
+                    // Report the location as collected and skip the default pickup
+                    ArchipelagoConsole.LogMessage("Reporting collection of " + Plugin.ItemIdToLocation[itemId]);
+                    Plugin.ArchipelagoClient.CollectFrom(Plugin.ItemIdToLocation[itemId]);
+                    return false;
+                }
+            }
+        }
+
+        // Proceed normally
+        return true;
+    }
+}
