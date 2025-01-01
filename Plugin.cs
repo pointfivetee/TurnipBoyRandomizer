60c9c63ba84ba7ca4257f1d1da9c099e75f8b52c
Matthew Peairs <25410392+pointfivetee@users.noreply.github.com>
2025-01-01 15:05:35 -0500

    Make the player start/continue a game before connecting

--- Plugin.cs
+++ Plugin.cs
@@ -13,7 +13,7 @@ public class Plugin : BaseUnityPlugin
 {
     public const string PluginGUID = "com.halftime.tbcte_ap";
     public const string PluginName = "Turnip Boy Commits Tax Evasion AP";
-    public const string PluginVersion = "0.1.0";
+    public const string PluginVersion = "0.1.1";
 
     public const string ModDisplayInfo = $"{PluginName} v{PluginVersion}";
     private const string APDisplayInfo = $"Archipelago v{ArchipelagoClient.APVersion}";
@@ -103,19 +103,31 @@ public class Plugin : BaseUnityPlugin
 
     private void OnGUI()
     {
-        // show the mod is currently loaded in the corner
-        GUI.Label(new Rect(16, 16, 300, 20), ModDisplayInfo);
+        var playerManager = Singleton<PlayerManager>.Instance;
+        var playerController = playerManager.GetConnectedPlayer();
+
         ArchipelagoConsole.OnGUI();
 
         string statusMessage;
         // show the Archipelago Version and whether we're connected or not
-        if (ArchipelagoClient.Authenticated)
+        if (! playerController)
+        {
+            // The player is still on the main menu
+            // Hide the cursor
+            Cursor.visible = false;
+
+            GUI.color = Color.black;
+            GUI.Label(new Rect(16, 10, 300, 20), ModDisplayInfo);
+            GUI.Label(new Rect(16, 30, 300, 25), "Start or continue a game to connect.");
+        }
+        else if (ArchipelagoClient.Authenticated)
         {
             // Hide the cursor now that we're connected
             Cursor.visible = false;
 
+            GUI.Label(new Rect(16, 10, 300, 20), ModDisplayInfo);
             statusMessage = " Status: Connected";
-            GUI.Label(new Rect(16, 50, 300, 20), APDisplayInfo + statusMessage);
+            GUI.Label(new Rect(16, 30, 300, 20), APDisplayInfo + statusMessage);
         }
         else
         {
@@ -123,21 +135,22 @@ public class Plugin : BaseUnityPlugin
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
 
+            GUI.Label(new Rect(16, 10, 300, 20), ModDisplayInfo);
             statusMessage = " Status: Disconnected";
-            GUI.Label(new Rect(16, 50, 300, 20), APDisplayInfo + statusMessage);
-            GUI.Label(new Rect(16, 70, 150, 20), "Host: ");
-            GUI.Label(new Rect(16, 90, 150, 20), "Player Name: ");
-            GUI.Label(new Rect(16, 110, 150, 20), "Password: ");
+            GUI.Label(new Rect(16, 30, 300, 20), APDisplayInfo + statusMessage);
+            GUI.Label(new Rect(16, 120, 150, 20), "Host: ");
+            GUI.Label(new Rect(16, 140, 150, 20), "Player Name: ");
+            GUI.Label(new Rect(16, 160, 150, 20), "Password: ");
 
-            ArchipelagoClient.ServerData.Uri = GUI.TextField(new Rect(150, 70, 150, 20),
+            ArchipelagoClient.ServerData.Uri = GUI.TextField(new Rect(150, 120, 150, 20),
                 ArchipelagoClient.ServerData.Uri);
-            ArchipelagoClient.ServerData.SlotName = GUI.TextField(new Rect(150, 90, 150, 20),
+            ArchipelagoClient.ServerData.SlotName = GUI.TextField(new Rect(150, 140, 150, 20),
                 ArchipelagoClient.ServerData.SlotName);
-            ArchipelagoClient.ServerData.Password = GUI.TextField(new Rect(150, 110, 150, 20),
+            ArchipelagoClient.ServerData.Password = GUI.TextField(new Rect(150, 160, 150, 20),
                 ArchipelagoClient.ServerData.Password);
 
             // requires that the player at least puts *something* in the slot name
-            if (GUI.Button(new Rect(16, 130, 100, 20), "Connect") &&
+            if (GUI.Button(new Rect(16, 180, 100, 20), "Connect") &&
                 !ArchipelagoClient.ServerData.SlotName.IsNullOrWhiteSpace())
             {
                 ArchipelagoClient.Connect();
@@ -145,10 +158,8 @@ public class Plugin : BaseUnityPlugin
         }
 
         // shift-R shortcut to die (to prevent potential softlocks)
-        if (Event.current.Equals(Event.KeyboardEvent("#R")))
+        if (Event.current.Equals(Event.KeyboardEvent("#R")) && playerController)
         {
-            var playerManager = Singleton<PlayerManager>.Instance;
-            var playerController = playerManager.GetConnectedPlayer();
             playerController.Die();
         }
     }
